b7817eb5fde859769b060f11b049a02525d88724
Chiktora <125090550+Chiktora@users.noreply.github.com>
2024-03-15 17:42:22 +0200

    Add project files.

---
+++ Simple Retail Management System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Simple_Retail_Management_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
